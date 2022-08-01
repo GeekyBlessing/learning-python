24954699f46af2f929f3760ae12a726fa8f122c2
GeekyBlessing <110393614+GeekyBlessing@users.noreply.github.com>
2022-08-01 14:36:57 +0100

    learnt about indentation in python

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello")
+def greeting():
+    print("starting server on port :8080...")
+
+    name = "mac"
+
+    print(name)
+
+greeting()
--- basics.py
+++ basics.py
@@ -1,1 +1,2 @@
-print("Hello World!")
+if 5 > 2:
+    print("five is greater then two!")
